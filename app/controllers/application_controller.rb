5181c8654fe5ab2e7ab7d7ac8ab90848a99c7178
Karl Glaser <kg@papercloud.com.au>
2014-02-02 04:34:06 +1100

    remove sqlite3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   def index
-  	@server = "http://cat-game.herokuapp.com"
+  	@server = ENV['SERVER'] || "localhost:3000"
   	render 'application'
   end
 end
